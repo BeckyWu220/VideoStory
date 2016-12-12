09113573963e5a309e91774d8f7933d45ce56c3a
Wanqiao Wu <wwq220@gmail.com>
2016-12-12 10:20:08 -0800

    Shoot video from camera.

--- StorytellingVideo/SelectImportViewController.swift
+++ StorytellingVideo/SelectImportViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import MediaPlayer
+import MobileCoreServices
 
 class SelectImportViewController: UIViewController {
     
     public var albumBtn: UIButton?
+    public var cameraBtn: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,11 @@ class SelectImportViewController: UIViewController {
         albumBtn?.backgroundColor = UIColor.gray
         self.view.addSubview(albumBtn!)
         
+        cameraBtn = UIButton(frame: CGRect(x: (self.view.frame.size.width - 100)/2, y: (self.view.frame.size.height - 30)/2 + 60, width: 100, height: 30))
+        cameraBtn?.setTitle("Camera", for: UIControlState.normal)
+        cameraBtn?.addTarget(self, action: #selector(clickCameraBtn), for: UIControlEvents.touchUpInside)
+        cameraBtn?.backgroundColor = UIColor.gray
+        self.view.addSubview(cameraBtn!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +41,95 @@ class SelectImportViewController: UIViewController {
     
     func clickAlbumBtn() -> Void {
         print("Click Album")
+        switchToMediaBrowser(viewController: self, usingDelegate: self)
+    }
+    
+    func clickCameraBtn() -> Void {
+        print("Click Camera")
+        switchCameraFromViewController(viewController: self, usingDelegate: self)
+    }
+    
+    func switchToMediaBrowser(viewController: UIViewController, usingDelegate delegate: UINavigationControllerDelegate & UIImagePickerControllerDelegate) -> Bool {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) == false{
+            return false
+        }
+        
+        let mediaUI = UIImagePickerController()
+        mediaUI.sourceType = .savedPhotosAlbum
+        mediaUI.mediaTypes = [kUTTypeMovie as NSString as String]
+        mediaUI.allowsEditing = true
+        mediaUI.delegate = delegate
+        
+        //self.navigationController?.pushViewController(mediaUI, animated: true)
+        present(mediaUI, animated: true, completion: nil)
+        return true
+    }
+    
+    func switchCameraFromViewController(viewController: UIViewController, usingDelegate delegate: UIImagePickerControllerDelegate & UINavigationControllerDelegate) -> Bool {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) == false {
+            return false
+        }
+        let cameraController = UIImagePickerController()
+        cameraController.sourceType = .camera
+        cameraController.mediaTypes = [kUTTypeMovie as NSString as String]
+        cameraController.allowsEditing = false
+        cameraController.delegate = delegate
+        
+        //self.navigationController?.pushViewController(cameraController, animated: true)
+        present(cameraController, animated: true, completion: nil)
+        return true
+    }
+    
+    func video(videoPath: NSString, didFinishSavingWithError error: NSError?, contextInfo info: Any) {
+        var title  = "Success"
+        var message = "Video was saved"
+        if let _ = error{
+            title = "Error"
+            message = "Video failed to save"
+        }
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 
+}
+
+extension SelectImportViewController: UIImagePickerControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        
+        
+        
+        dismiss(animated: true, completion: {
+            if mediaType == kUTTypeMovie{
+                
+                guard let path = (info[UIImagePickerControllerMediaURL] as! NSURL).path else {
+                    return
+                }
+                
+                if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(path){
+                    UISaveVideoAtPathToSavedPhotosAlbum(path, self, #selector(SelectImportViewController.video(videoPath:didFinishSavingWithError:contextInfo:)), nil)
+                }
+                
+                let asset = AVURLAsset(url: (info[UIImagePickerControllerMediaURL] as! NSURL) as URL!)
+                let imgGenerator = AVAssetImageGenerator(asset: asset)
+                let image : UIImage = try! UIImage(cgImage: imgGenerator.copyCGImage(at: CMTimeMake(0, 1), actualTime: nil))
+                
+                //self.currentVideoSection?.initWithColor(color: UIColor.gray)
+//                self.currentVideoSection?.videoIcon?.image = image
+//                self.currentVideoSection?.containVideo = true
+//                self.currentVideoSection?.videoURL = (info[UIImagePickerControllerMediaURL] as! NSURL) as URL!
+                
+                /*let moviePlayer = MPMoviePlayerViewController(contentURL: (info[UIImagePickerControllerMediaURL] as! NSURL) as URL!)
+                 self.presentMoviePlayerViewControllerAnimated(moviePlayer)*/
+                
+                
+            }
+        })
+    }
+}
+
+extension SelectImportViewController: UINavigationControllerDelegate{
+    
 }
--- StorytellingVideo/ViewController.swift
+++ StorytellingVideo/ViewController.swift
@@ -51,26 +51,10 @@ class ViewController: UIViewController, VideoSectionDelegate {
             self.presentMoviePlayerViewControllerAnimated(moviePlayer)
         }else{
             currentVideoSection = videoSection
-            //startMediaBrowserFromViewController(viewController: self, usingDelegate: self)
             
             let importController : SelectImportViewController = SelectImportViewController()
             self.navigationController?.pushViewController(importController, animated: true)
         }
-    }
-    
-    func startMediaBrowserFromViewController(viewController: UIViewController, usingDelegate delegate: UINavigationControllerDelegate & UIImagePickerControllerDelegate) -> Bool{
-        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) == false{
-            return false
-        }
-        
-        let mediaUI = UIImagePickerController()
-        mediaUI.sourceType = .savedPhotosAlbum
-        mediaUI.mediaTypes = [kUTTypeMovie as NSString as String]
-        mediaUI.allowsEditing = true
-        mediaUI.delegate = delegate
-        
-        present(mediaUI, animated: true, completion: nil)
-        return true
     }
 
     override func didReceiveMemoryWarning() {
@@ -80,28 +64,6 @@ class ViewController: UIViewController, VideoSectionDelegate {
     
 }
 
-extension ViewController: UIImagePickerControllerDelegate{
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
-        
-        dismiss(animated: true, completion: {
-            if mediaType == kUTTypeMovie{
-                
-                let asset = AVURLAsset(url: (info[UIImagePickerControllerMediaURL] as! NSURL) as URL!)
-                let imgGenerator = AVAssetImageGenerator(asset: asset)
-                let image : UIImage = try! UIImage(cgImage: imgGenerator.copyCGImage(at: CMTimeMake(0, 1), actualTime: nil)) 
-            
-                //self.currentVideoSection?.initWithColor(color: UIColor.gray)
-                self.currentVideoSection?.videoIcon?.image = image
-                self.currentVideoSection?.containVideo = true
-                self.currentVideoSection?.videoURL = (info[UIImagePickerControllerMediaURL] as! NSURL) as URL!
-                
-                /*let moviePlayer = MPMoviePlayerViewController(contentURL: (info[UIImagePickerControllerMediaURL] as! NSURL) as URL!)
-                self.presentMoviePlayerViewControllerAnimated(moviePlayer)*/
-            }
-        })
-    }
-}
 
 extension ViewController: UINavigationControllerDelegate{
     
